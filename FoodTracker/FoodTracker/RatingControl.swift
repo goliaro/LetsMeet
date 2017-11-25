bd0dbc8050013eb166e06d738cd5558e52489d6e
Gabriele Oliaro <ict@gabrieleoliaro.it>
2017-11-25 13:44:07 -0500

    before lunch

--- FoodTracker/FoodTracker/RatingControl.swift
+++ FoodTracker/FoodTracker/RatingControl.swift
@@ -40,16 +40,52 @@ import UIKit
     
     //MARK: Button Action
     func ratingButtonTapped(button: UIButton) {
-        print("Button pressed 👍")
+        guard let index = ratingButtons.index(of: button) else {
+            fatalError("The button, \(button), is not in the ratingButtons array: \(ratingButtons)")
+        }
+        
+        // Calculate the rating of the selected button
+        let selectedRating = index + 1
+        if selectedRating == rating
+        {
+            // If the selected star represents the current rating, reset the rating to 0.
+            rating = 0
+        }
+        else
+        {
+            // Otherwise set the rating to the selected star
+            rating = selectedRating
+        }
     }
     
     //MARK: Private Methods
     private func setupButtons() {
         
-        for _ in 0..<starCount {
+        // clear any existing buttons
+        for button in ratingButtons {
+            removeArrangedSubview(button)
+            button.removeFromSuperview()
+        }
+        ratingButtons.removeAll()
+        
+        // Load Button Images
+        let bundle = Bundle(for: type(of: self))
+        let filledStar = UIImage(named: "filledStar", in: bundle, compatibleWith: self.traitCollection)
+        let emptyStar = UIImage(named:"emptyStar", in: bundle, compatibleWith: self.traitCollection)
+        let highlightedStar = UIImage(named:"highlightedStar", in: bundle, compatibleWith: self.traitCollection)
+        
+        
+        for _ in 0..<starCount
+        {
+            
             // Create the button
             let button = UIButton()
-            button.backgroundColor = UIColor.red
+            
+            // Set the button images
+            button.setImage(emptyStar, for: .normal)
+            button.setImage(filledStar, for: .selected)
+            button.setImage(highlightedStar, for: .highlighted)
+            button.setImage(highlightedStar, for: [.highlighted, .selected])
         
             // Add constraints
             button.translatesAutoresizingMaskIntoConstraints = false
@@ -60,8 +96,11 @@ import UIKit
             button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
         
         
-            // Add the buttn to the stack
+            // Add the button to the stack
             addArrangedSubview(button)
+            
+            // Add the new button to the rating button array
+            ratingButtons.append(button)
         }
         
     }
