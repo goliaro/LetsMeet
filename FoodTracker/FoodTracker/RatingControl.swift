b50d0c1d7b1526ca27d8362b69e13939435c2ed3
Gabriele Oliaro <ict@gabrieleoliaro.it>
2017-11-25 12:38:46 -0500

    first commit

---
+++ FoodTracker/FoodTracker/RatingControl.swift
@@ -0,0 +1,40 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Oliaro, Gabriele on 11/25/17.
+//  Copyright © 2017 Oliaro, Gabriele. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    //MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        setupButtons()
+    }
+    
+    //MARK: Private Methods
+    private func setupButtons() {
+        
+        // Create the button
+        let button = UIButton()
+        button.backgroundColor = UIColor.red
+        
+        // Add constraints
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        
+        // Add the buttn to the stack
+        addArrangedSubview(button)
+        
+    }
+
+}
