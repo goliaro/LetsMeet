3532f09da07915c137bffe7a65b3f720e2e60ca2
Gabriele Oliaro <gabriele_oliaro@college.harvard.edu>
2017-11-25 20:55:00 -0500

    defined data model defined test for current data model

---
+++ FoodTracker/FoodTracker/Meal.swift
@@ -0,0 +1,41 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Oliaro, Gabriele on 11/25/17.
+//  Copyright © 2017 Oliaro, Gabriele. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    
+    //MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    //MARK: Initialization
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        
+        // The name must not be empty
+        guard !name.isEmpty else {
+            return nil
+        }
+        
+        // The rating must be between 0 and 5 inclusively
+        guard (rating >= 0) && (rating <= 5) else {
+            return nil
+        }
+
+        
+        // Initialize stored properties.
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        
+        
+    }
+}
