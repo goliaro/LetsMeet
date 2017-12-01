bc338d96b52d6a46c9e913dfdd5a35673bea7a82
Gabriele Oliaro <mail@gabrieleoliaro.it>
2017-11-30 21:32:52 -0500

    create final folder

---
+++ Let's Meet/Let's Meet/GroupTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  GroupTableViewCell.swift
+//  Let's Meet
+//
+//  Created by Oliaro, Gabriele on 11/30/17.
+//  Copyright © 2017 Kit, Alejandro & Gabriel. All rights reserved.
+//
+
+
+
+/* What does this class do:
+ this class manages each cell of the table view in the groups page. It is called "GroupTableViewCell" because each cell shows a group, just like in Whatsapp.
+ */
+
+import UIKit
+
+class GroupTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
