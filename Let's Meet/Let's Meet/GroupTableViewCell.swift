fff467fc6f4d298bbc4472eb0ba80b1909a1ea49
Gabriele Oliaro <mail@gabrieleoliaro.it>
2017-12-02 12:08:55 -0500

    backup

--- Let's Meet/Let's Meet/GroupTableViewCell.swift
+++ Let's Meet/Let's Meet/GroupTableViewCell.swift
@@ -15,6 +15,18 @@
 import UIKit
 
 class GroupTableViewCell: UITableViewCell {
+    
+    
+    /* 
+     The following section is responsible for creating the outlets of the photo, title and description elements of every cell in the table view. As per the official Apple Developer Guide "Start Developing iOS Apps (Swift):
+ 
+     "Before you can display dynamic data in your table view cells, you need to create outlet connections between the prototype in your storyboard and the code that represents the table view cell in MealTableViewCell.swift."
+     */
+    //MARK: Properties
+    @IBOutlet weak var groupNameLabel: UILabel!
+    @IBOutlet weak var groupDescriptionLabel: UILabel!
+    @IBOutlet weak var groupPhotoImageView: UIImageView!
+    
 
     override func awakeFromNib() {
         super.awakeFromNib()
