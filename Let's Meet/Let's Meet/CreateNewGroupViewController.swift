c089d537ef6939207e258e53ffedb58826c8d47d
Gabriele Oliaro <gabriele@Gabrieles-iMac.local>
2017-12-06 22:24:26 -0500

    backup4

---
+++ Let's Meet/Let's Meet/CreateNewGroupViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CreateNewGroupViewController.swift
+//  Let's Meet
+//
+//  Created by Gabriele Oliaro on 12/6/17.
+//  Copyright © 2017 Kit, Alejandro & Gabriel. All rights reserved.
+//
+
+import UIKit
+
+class CreateNewGroupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
