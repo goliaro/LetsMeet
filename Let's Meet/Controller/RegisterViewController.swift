6f04cb866f96e0835648613e2aad512099b3d0a1
Gabriele Oliaro <gabriele_oliaro@college.harvard.edu>
2019-01-04 17:22:09 +0100

    implemented remove photo button on register page

--- Let's Meet/Controller/RegisterViewController.swift
+++ Let's Meet/Controller/RegisterViewController.swift
@@ -176,6 +176,9 @@ class RegisterViewController: UIViewController, UITextFieldDelegate, UIScrollVie
     }
     
     @IBAction func removeImage(_ sender: UIButton) {
+        // remove image from imageview preview
+        let noimage: UIImage = UIImage(named: "defaultPhoto")!
+        profilePhotoImageView.image = noimage
         
     }
     
@@ -197,7 +200,7 @@ class RegisterViewController: UIViewController, UITextFieldDelegate, UIScrollVie
         // if survived
         // set image into imageview for a quick preview
         profilePhotoImageView.contentMode = .scaleAspectFit
-        profilePhotoImageView.image = pickedImage
+        profilePhotoImageView.image = pi
         
         // save image into globals so that we can access it
         pickedImage = pi
@@ -211,38 +214,6 @@ class RegisterViewController: UIViewController, UITextFieldDelegate, UIScrollVie
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         dismiss(animated: true, completion: nil)
     }
-    
-    //MARK: UITextFieldDelegate
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        // #cs50 Hide the keyboard
-        textField.resignFirstResponder()
-        
-        if (textField.tag > 2)
-        {
-            // Scroll back up to show all the elements in the page
-            scrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: true)
-        }
-        return true
-        
-    }
-    
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        // #cs50 When the user starts typing, scroll down the page using a scroll view, so that no textfield is covered by the keyboard.
-        // If the selected text field is the password or the confirm password one (We tagged them from 1 to 4 in order), scroll down so that the text field is not hidden by the keyboard
-        if (textField.tag > 2) {
-            scrollView.setContentOffset(CGPoint(x: 0, y: 200), animated: true)
-        }
-    }
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
     
     
 }
